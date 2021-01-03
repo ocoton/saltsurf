3d10c680a8ad805eaeae8da949d3a11b2d1b3dbc
oliver coton <oliver.coton@gmail.com>
2021-01-03 14:10:03 +0100

    changed stormglass api service back to 7 days

--- app/services/get_stormglass_api_service.rb
+++ app/services/get_stormglass_api_service.rb
@@ -10,7 +10,7 @@ class GetStormglassApiService
     lat = @spot.latitude;
     lng =  @spot.longitude;
     source = 'sg,meteo,noaa';
-    end_time = Time.now.utc.beginning_of_day + 1.days;
+    end_time = Time.now.utc.beginning_of_day + 7.days;
     params = 'waveHeight,swellDirection,swellHeight,swellPeriod,waveDirection,waveHeight,wavePeriod,windDirection,windSpeed,airTemperature,seaLevel,waterTemperature';
     response = HTTParty.get("https://api.stormglass.io/v2/weather/point?lat=#{lat}&lng=#{lng}&params=#{params}&source=#{source}&end=#{end_time}",
       headers: {
@@ -25,7 +25,7 @@ class GetStormglassApiService
       wave_period_us = hour["wavePeriod"]["noaa"].to_f
       wind_speed_us = hour["windSpeed"]["noaa"].to_f
       rating_us = get_rating(wave_height_us, wave_period_us, wind_speed_us)
-      
+
       Forecast.create!(
         spot: @spot,
         timestamp: hour["time"],
@@ -43,8 +43,8 @@ class GetStormglassApiService
         air_temperature: hour["airTemperature"]["sg"],
         water_temperature:hour["waterTemperature"]["sg"],
       )
-  
-     # get rating for Meteo 
+
+     # get rating for Meteo
       wave_height_mf = hour["waveHeight"]["meteo"].to_f
       wave_period_mf = hour["wavePeriod"]["meteo"].to_f
       wind_speed_mf = hour["windSpeed"]["meteo"].to_f
@@ -86,7 +86,7 @@ class GetStormglassApiService
         wave_rating = 1
     end
 
-    
+
     if wave_period <= 5
         period_rating = -3
     elsif wave_period <= 7
@@ -97,7 +97,7 @@ class GetStormglassApiService
         period_rating = 0
     end
 
-    
+
     if wind_speed <= 12
         wind_rating = 0
     elsif wind_speed <= 15
